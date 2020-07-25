3876aeb0edfee86d9d4b115a92536825ea896ff7
luk6xff <lukasz.uszko@gmail.com>
2020-07-25 02:10:32 +0200

    demo rtos task

--- esp32-app/src/main.cpp
+++ esp32-app/src/main.cpp
@@ -1,9 +1,60 @@
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <driver/gpio.h>
+#include "sdkconfig.h"
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+/* Can run 'make menuconfig' to choose the GPIO to blink,
+   or you can edit the following line and set a number here.
+*/
+#define BLINK_GPIO (gpio_num_t)CONFIG_BLINK_GPIO
+
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 4
+#endif
+
+void blink_task(void *pvParameter)
+{
+    gpio_pad_select_gpio(BLINK_GPIO);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    while(1) {
+        /* Blink off (output low) */
+        gpio_set_level(BLINK_GPIO, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        /* Blink on (output high) */
+        gpio_set_level(BLINK_GPIO, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+#if !CONFIG_AUTOSTART_ARDUINO
+void arduinoTask(void *pvParameter) {
+    pinMode(LED_BUILTIN, OUTPUT);
+    while(1) {
+        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+        delay(1000);
+    }
 }
+
+void app_main()
+{
+    // initialize arduino library before we start the tasks
+    initArduino();
+
+    xTaskCreate(&blink_task, "blink_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
+    xTaskCreate(&arduinoTask, "arduino_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
+}
+#else
+void setup() {
+    Serial.begin(9600);
+    xTaskCreate(&blink_task, "blink_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+void loop() {
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    Serial.println("Hello!");
+    delay(1000);
+}
+#endif
