d4362f59be8c7c31fe9f0e0c931ed8f64133f21c
luk6xff <lukasz.uszko@gmail.com>
2020-10-16 16:46:18 +0200

    Display Task sync

---
+++ esp32-app/src/App/rtos_common.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/semphr.h>
+
+namespace rtos
+{
+
+/**
+ * @brief Simple RAII lock_guard implementation
+ */
+template<typename T>
+class LockGuard
+{
+
+public:
+    LockGuard(T& handle)
+        : m_handle(handle)
+    {
+        m_handle.lock();
+    }
+
+    ~LockGuard()
+    {
+        m_handle.unlock();
+    }
+
+    LockGuard(const LockGuard& other) = delete;
+    LockGuard(LockGuard&& other) = delete; 
+    LockGuard& operator=(const LockGuard& other) = delete; 
+
+private:
+    T& m_handle;
+};
+
+
+/**
+ * @brief FreeRtos mutex wrapper
+ */
+class Mutex final
+{
+
+public: 
+    Mutex();
+    ~Mutex();
+
+    void lock();
+    void unlock();
+    void reset();
+
+private:
+    SemaphoreHandle_t m_handle;
+};
+
+
+} // rtos
+
+/**
+ * @brief Globally used rtos objects
+ */
+extern rtos::Mutex i2cMutex;
