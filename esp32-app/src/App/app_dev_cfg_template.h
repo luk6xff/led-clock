e9d4c1c87bfa635f772069bae91f003c49c6919c
luk6xff <lukasz.uszko@gmail.com>
2020-10-24 01:53:18 +0200

    Owm api and task done

---
+++ esp32-app/src/App/app_dev_cfg_template.h
@@ -0,0 +1,11 @@
+#pragma once
+/**
+ * @brief Test only development default cfg data - template
+ */
+
+// WIFI
+#define DEV_CFG_WIFI_SSID ""
+#define DEV_CFG_WIFI_PASS ""
+
+// WEATHER
+#define DEV_CFG_WEATHER_OWM_APPID ""
