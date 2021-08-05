a122951f1e3650aeedbc21aadedd18ae8d950498
luk6xff <lukasz.uszko@gmail.com>
2021-08-05 23:45:28 +0200

    Release v1.1

--- esp32-app/src/App/app_vars.h
+++ esp32-app/src/App/app_vars.h
@@ -9,4 +9,4 @@
 // Define the app version number
 #define APP_VERSION "0.1.0"
 
-#define APP_DATE_LAST_MODIFIED "Fri, 23 Jul 2021 19:16:29 GMT"
+#define APP_DATE_LAST_MODIFIED "Thu, 05 Aug 2021 21:44:55 GMT"
